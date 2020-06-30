c92763701d33af4e5dff4c3269face425834568b
EijiKajiwara <kkajiei@gmail.com>
2020-06-30 12:33:03 +0900

    second commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,9 +8,9 @@ class ApplicationController < ActionController::Base
   class Forbidden < StandardError; end
 
   if Rails.env.production? || ENV["RESCUE_EXCEPTIONS"]
-    rescue_from StandardError, with: rescue_internal_server_error
-    rescue_from ActiveRecord::RecordNotFound, with: rescue_not_found
-    rescue_from ActionController::ParameterMissing, with: rescue_bad_request
+    rescue_from StandardError, with: :rescue_internal_server_error
+    rescue_from ActiveRecord::RecordNotFound, with: :rescue_not_found
+    rescue_from ActionController::ParameterMissing, with: :rescue_bad_request
   end
 
   rescue_from LoginRequired, with: :rescue_login_required
@@ -34,12 +34,12 @@ class ApplicationController < ActionController::Base
     render "errors/forbidden", status: 403, layout: "error",
       formats: [:html]
   end
-  
+
   private def rescue_not_found(exception)
     render "errors/not_found", status: 404, layout: "error",
       formats: [:html]
   end
-  
+
   private def rescue_internal_server_error(exception)
     render "errors/internal_server_error", status: 500, layout: "error",
       formats: [:html]
